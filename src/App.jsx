5316a9754d9d73fe00582b253f98797c27cf0881
milindkusahu <milindsahu2000@gmail.com>
2024-10-18 18:34:37 +0530

    Dark mode with system, light, dark radio buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Box, Button, Container, Paper, Typography } from "@mui/material";
+import DarkModeToggle from "./components/DarkModeToggle";
 
-function App() {
-  const [count, setCount] = useState(0)
+const serviceList = ["Service 1", "Service 2", "Service 3"];
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container>
+      <DarkModeToggle />
+      <Typography
+        variant="h1"
+        sx={{ my: 4, textAlign: "center", color: "primary.main" }}
+      >
+        My Cards
+      </Typography>
+      <Typography variant="h2">Overview</Typography>
+      <Box
+        sx={{
+          pt: 4,
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: "space-between",
+          gap: 4,
+        }}
+      >
+        {serviceList.map((ele, index) => (
+          <Paper elevation={3} sx={{ width: { xs: 1, md: 320 } }} key={index}>
+            <Box sx={{ m: 3 }}>
+              <Typography variant="h3">{ele}</Typography>
+              <Typography sx={{ mt: 2 }}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+                laborum molestias magni, autem minima, perferendis ea atque qui
+                et voluptates id ipsam veniam quae facilis quia non voluptas
+                praesentium aperiam!
+              </Typography>
+              <Button variant="contained" color="secondary" sx={{ mt: 2 }}>
+                Learn More
+              </Button>
+            </Box>
+          </Paper>
+        ))}
+      </Box>
+    </Container>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
+export const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+  palette: {
+    primary: {
+      main: "#013e87",
+    },
+    secondary: {
+      main: "#2e74c9",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: "600",
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: "600",
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: "600",
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
